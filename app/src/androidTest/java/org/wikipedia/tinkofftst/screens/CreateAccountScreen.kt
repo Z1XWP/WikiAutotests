ee815570ad0b70129917565d2398032bb736e43a
Z1XWP <vladptula@gmail.com>
2023-07-31 18:32:44 +0300

    Добавлены матчеры и сделан тест с AllureId = 4

--- app/src/androidTest/java/org/wikipedia/tinkofftst/screens/CreateAccountScreen.kt
+++ app/src/androidTest/java/org/wikipedia/tinkofftst/screens/CreateAccountScreen.kt
@@ -9,15 +9,23 @@ import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Checks
 import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers.hasSibling
+import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isFocused
 import androidx.test.espresso.matcher.ViewMatchers.withHint
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.google.android.material.internal.CheckableImageButton
 import com.google.android.material.textfield.TextInputLayout
 import junit.framework.TestCase.assertTrue
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.instanceOf
 import org.wikipedia.R
+import org.wikipedia.tinkofftst.matchers.PasswordTransformationMethodMatchers.hasNotPasswordTransformation
+import org.wikipedia.tinkofftst.matchers.PasswordTransformationMethodMatchers.hasPasswordTransformation
+import org.wikipedia.tinkofftst.matchers.TextColorMatchers.withTextColor
 import org.wikipedia.util.ResourceUtil.getThemedColor
 
 
@@ -28,6 +36,14 @@ class CreateAccountScreen {
     private val emailEditText by lazy { onView(withHint(R.string.create_account_email_hint)) }
     private val nextButton by lazy { onView(withId(R.id.create_account_submit_button)) }
     private val errorMessageTextView by lazy { onView(withId(R.id.create_account_password_input)) }
+    private val obfuscatePasswordButton by lazy {
+        onView(
+            allOf(
+                isDescendantOfA(hasSibling(isFocused())),
+                withId(PASSWORD_EYE_BUTTON_ID)
+            )
+        )
+    }
 
     fun typeUsername(username: String = DEFAULT_USERNAME) {
         usernameEditText.perform(
@@ -57,13 +73,16 @@ class CreateAccountScreen {
         nextButton.perform(click())
     }
 
+    fun clickObfuscatePasswordButton() {
+        obfuscatePasswordButton.perform(click())
+    }
+
     fun checkPasswordIsObfuscated() {
-        passwordEditText.check { view, _ ->
-            assertTrue(
-                "PasswordTransformationMethod should be set",
-                (view as EditText).transformationMethod is PasswordTransformationMethod
-            )
-        }
+        passwordEditText.check(matches(hasPasswordTransformation()))
+    }
+
+    fun checkPasswordIsNotObfuscated() {
+        passwordEditText.check(matches(hasNotPasswordTransformation()))
     }
 
     fun checkErrorMessageColor() {
@@ -77,26 +96,11 @@ class CreateAccountScreen {
         )
     }
 
-    private fun withTextColor(expectedColor: Int): Matcher<View?> {
-        Checks.checkNotNull(expectedColor)
-        return object : BoundedMatcher<View?, TextInputLayout>(TextInputLayout::class.java) {
-            override fun matchesSafely(item: TextInputLayout): Boolean {
-                val convertedColor = getThemedColor(item.context, expectedColor)
-                return convertedColor == item.errorCurrentTextColors
-            }
-
-            override fun describeTo(description: Description) {
-                description.appendText("with text color: ")
-                description.appendValue(expectedColor)
-            }
-        }
-    }
-
-
     companion object {
         private const val DEFAULT_USERNAME = "Smth.username"
         private const val DEFAULT_PASSWORD = "pass12"
         private const val DEFAULT_EMAIL = "smth.email@gmail.com"
+        private const val PASSWORD_EYE_BUTTON_ID = 2131297700
         inline operator fun invoke(crossinline block: CreateAccountScreen.() -> Unit) {
             CreateAccountScreen().block()
         }
